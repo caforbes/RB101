2d6b08e3866a3ec269ba4021ce6df1495500c9fa
Clarissa Forbes <for.c.forbes@gmail.com>
2020-08-04 10:27:26 -0700

    L6 ttt walkthrough

---
+++ lesson_6/tictactoe.rb
@@ -0,0 +1,121 @@
+require 'pry'
+
+EMPTY_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(msg)
+  puts ">> #{msg}"
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = EMPTY_MARKER}
+  new_board
+end
+
+def display_board(brd)
+  system 'cls'
+  puts "Player is #{PLAYER_MARKER}; Computer is #{COMPUTER_MARKER}."
+  puts ""
+  puts "     |     |     "
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
+  puts "     |     |     "
+  puts "-----+-----+-----"
+  puts "     |     |     "
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}  "
+  puts "     |     |     "
+  puts "-----+-----+-----"
+  puts "     |     |     "
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}  "
+  puts "     |     |     "
+  puts ""
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == EMPTY_MARKER}
+end
+
+def player_move!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+
+    break if empty_squares(brd).include?(square)
+    prompt "That's not a valid square to move!"
+  end
+
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_move!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  # returns string(player/computer) or nil
+  winning_lines = [
+    [1, 2, 3], # rows
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7], # columns
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9], #diagonals
+    [3, 5, 7]
+  ]
+
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+      brd[line[1]] == PLAYER_MARKER &&
+      brd[line[2]] == PLAYER_MARKER
+      return 'Player'
+    elsif brd[line[0]] == COMPUTER_MARKER &&
+      brd[line[1]] == COMPUTER_MARKER &&
+      brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+prompt "Welcome to TIC-TAC-TOE! Press enter to begin."
+gets.chomp
+
+loop do
+  board = initialize_board
+
+  loop do
+    display_board(board)
+
+    player_move!(board)
+    break if someone_won?(board)
+
+    computer_move!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase == 'y'
+end
+
+prompt "Thanks for playing TIC-TAC-TOE! Goodbye!"

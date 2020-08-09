6bde860f5b1bce26ae2ab0a6054a83047a97968b
Clarissa Forbes <for.c.forbes@gmail.com>
2020-08-09 12:30:35 -0700

    L6 ttt review: gameplay refactor

--- lesson_6/tictactoe2.rb
+++ lesson_6/tictactoe2.rb
@@ -2,13 +2,15 @@ EMPTY_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 
+MIDDLE_SQUARE = 5
 WINNING_LINES = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9], # rows
   [1, 4, 7], [2, 5, 8], [3, 6, 9], # columns
   [1, 5, 9], [3, 5, 7] # diagonals
 ]
 
-TURN_OPTION = 'Choose' # Player, Computer, Choose, Alternate, Loser
+TURN_OPTION = 'Alternate' # Player, Computer, Choose, Alternate, Loser
+WINS_TO_VICTORY = 5
 
 def prompt(msg)
   puts ">> #{msg}"
@@ -57,9 +59,10 @@ def player_move!(brd)
   square = ''
   loop do
     prompt "Choose a square (#{joinor(empty_squares(brd))}):"
-    square = gets.chomp.to_i
+    square = gets.chomp
+    square = square.to_i if square == square.to_i.to_s
 
-    break if empty_squares(brd).include?(square)
+    break if  empty_squares(brd).include?(square)
     prompt "That's not a valid square to move!"
   end
 
@@ -72,7 +75,7 @@ def computer_move!(brd)
 
   square =  if attack_option then attack_option
             elsif defense_option then defense_option
-            elsif brd[5] == EMPTY_MARKER then 5
+            elsif brd[MIDDLE_SQUARE] == EMPTY_MARKER then MIDDLE_SQUARE
             else empty_squares(brd).sample
             end
 
@@ -154,7 +157,8 @@ def alternate_player(player)
 end
 
 prompt "Welcome to TIC-TAC-TOE!"
-prompt "The first to win five rounds (you or the computer) will win the game!"
+prompt "It's you versus the computer."
+prompt "The first to win #{WINS_TO_VICTORY} rounds wins the game!"
 prompt "Press enter to begin."
 gets.chomp
 
@@ -190,19 +194,20 @@ loop do
     computer_score = update_scores(player_score, computer_score, board)
   display_scores(player_score, computer_score)
 
-  if player_score == 5
+  if player_score == WINS_TO_VICTORY
     prompt ""
     prompt "YOU WON THE GAME!!!!"
     break
-  elsif computer_score == 5
+  elsif computer_score == WINS_TO_VICTORY
     prompt ""
     prompt "The computer beat you!!!!"
     break
   end
 
-  prompt "Play the next game? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase == 'y'
+  prompt "Press enter to play the next game. (q to quit)"
+  quit = gets.chomp.downcase
+
+  break if ['q', 'quit'].include?(quit)
 
   case TURN_OPTION
   when 'Choose' then plays_first = select_first_player

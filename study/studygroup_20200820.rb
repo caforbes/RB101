c3a3554b467ef8f5db4dd5444d593ab1246d1aa6
Clarissa Forbes <for.c.forbes@gmail.com>
2020-08-23 10:30:49 -0700

    study group: permutations method problem

--- study/studygroup_20200820.rb
+++ study/studygroup_20200820.rb
@@ -33,16 +33,77 @@ def next_bigger(num)
   digits = num.digits.reverse
   return -1 if num == max_permutation(num)
 
-  possible_next_nums = digits.permutation.to_a.map { |order| order.join.to_i }
+  possible_next_nums = permutations(digits).map { |order| order.join.to_i }
   possible_next_nums = possible_next_nums.uniq.sort
 
   current_num = possible_next_nums.index(num)
   possible_next_nums[current_num + 1]
 end
 
+def next_bigger_ta(num)
+  digits = num.digits.reverse
+  return -1 if num == max_permutation(num)
+
+  current_num = num
+  loop do
+    current_num += 1
+    return current_num if max_permutation(current_num) == max_permutation(num)
+  end
+end
+
 def max_permutation(num)
   num.digits.sort.reverse.join.to_i
 end
+
+def permutations(array)
+  results = []
+
+  if array.length == 1
+    results << array.dup
+  else
+    previous_perms = permutations(array[0...-1])
+    last_element = array[-1]
+
+    previous_perms.each do |current_perm|
+      results += insert_at_each_space(current_perm, last_element)
+    end
+  end
+
+  results.sort
+end
+
+def insert_at_each_space(array, new_element)
+  results = []
+
+  (array.length + 1).times do |index|
+    new_arr = array.dup
+    new_arr.insert(index, new_element)
+    results << new_arr
+  end
+
+  results.sort
+end
+
+  # initialize *results* array of permutations (nested array)
+  # perms of 1-el array
+    # copy of array
+    # add copy to result array
+  # perms of 2-el array
+    # if array has size > 1
+    # get permutations of array sans last element (recurse)
+    # then iterate over the permutations
+    # for each permutation
+      # loop: insert last element in first position of (copy of) the array (ind 0)
+      # add that array to results array
+      # repeat for every position in the array
+  # return SORTED array of arrays (permutations)
+
+# p insert_at_each_space([1], 2) == [[1,2], [2,1]]
+# p insert_at_each_space([1,2], 3) == [[1,2,3], [1,3,2], [3,1,2]]
+
+# p permutations([1]) == [[1]].sort
+# p permutations([1,2]) == [[1,2], [2,1]].sort
+# p permutations([1,2,3]) == [[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]].sort
 
 p next_bigger(12) == 21
 p next_bigger(513) == 531
@@ -52,3 +113,12 @@ p next_bigger(144) == 414
 p next_bigger(9) == -1
 p next_bigger(111) == -1
 p next_bigger(531) == -1
+
+p next_bigger_ta(12) == 21
+p next_bigger_ta(513) == 531
+p next_bigger_ta(2017) == 2071
+p next_bigger_ta(414) == 441
+p next_bigger_ta(144) == 414
+p next_bigger_ta(9) == -1
+p next_bigger_ta(111) == -1
+p next_bigger_ta(531) == -1

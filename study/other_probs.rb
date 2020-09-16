2b40f89f62ea80ffb0ff5b47ca339a514b106db7
caforbes <for.c.forbes@gmail.com>
2020-09-16 10:55:58 -0700

    final WOC video problem

--- study/other_probs.rb
+++ study/other_probs.rb
@@ -341,8 +341,7 @@ scramble?('scriptingjava', 'javascript') == true
 def longest_palindrome(string)
   return 0 if string.empty?
   palindromes = substrings(string).select { |substr| palindrome?(substr) }
-  palindromes.map!(&:length)
-  palindromes.max
+  palindromes.max_by(&:length).length
 end
 
 def substrings(string)
@@ -371,12 +370,48 @@ end
 #                           'lo', 'o'
 #                           ]
 
-p longest_palindrome("") == 0
-p longest_palindrome("a") == 1
-p longest_palindrome("aa") == 2
-p longest_palindrome("baa") == 2
-p longest_palindrome("aab") == 2
-p longest_palindrome("baabcd") == 4
-p longest_palindrome("baablkj12345432133d") == 9
+longest_palindrome("") == 0
+longest_palindrome("a") == 1
+longest_palindrome("aa") == 2
+longest_palindrome("baa") == 2
+longest_palindrome("aab") == 2
+longest_palindrome("baabcd") == 4
+longest_palindrome("baablkj12345432133d") == 9
 
 # ~23m
+
+
+
+=begin
+- take an array of integers (pos/neg)
+- return index at which elements on both sides have equal sums
+- don't count the value of element at that index
+- if a balanced index can't be found return -1
+
+- given an index, get sum of elements before and after
+  - get two sub-arrays of an array based on an index: before/after elements
+    - first half: [0...ind]
+    - second half: [ind+1..-1]
+  - transform subarrays to their sums
+- if before/after sums are equal, return the index
+- if the end is reached without equivalence, return -1
+=end
+
+def find_even_index(array)
+  array.each_index do |index|
+    before_sum = array[0...index].sum
+    after_sum = array[index+1..-1].sum
+    return index if before_sum == after_sum
+  end
+  -1
+end
+
+p find_even_index([1,2,3,4,3,2,1]) == 3
+p find_even_index([1,100,50,-51,1,1]) == 1
+p find_even_index([1,2,3,4,5,6]) == -1
+p find_even_index([20,10,30,10,10,15,35]) == 3
+p find_even_index([20,10,-80,10,10,15,35]) == 0
+p find_even_index([10,-80,10,10,15,35,20]) == 6
+p find_even_index([-1,-2,-3,-4,-3,-2,-1]) == 3
+
+# ~20m
